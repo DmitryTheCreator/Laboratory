e6317798926630b60ea90bd1ead184d0427027a5
DmitryTheCreator <dmitryzamorev@gmail.com>
2022-05-05 21:53:43 +0500

    Класс сотрудника

---
+++ Laboratory/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace Laboratory.Models
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public bool Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public string MaritalStatus { get; set; }
+
+        public bool HavingChildren { get; set; }
+
+        public string Post { get; set; }
+
+        public string Degree { get; set; }
+    }
+}
